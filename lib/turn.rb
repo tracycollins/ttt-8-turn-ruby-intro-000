9157f2dc11a0cd49c1200e711c350a15ede1400b
Tracy Collins <tc@threeceemedia.com>
2020-02-18 01:46:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ end
 def valid_move?(board, index)
   if (index < 0 || index > 8)
     false
-  elsif position_taken?(board, index)
+  elsif (position_taken?(board, index))
     false
   else
     true
